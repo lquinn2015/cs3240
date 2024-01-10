f36e325953837737555240d4896ad4a092eb4e6c
Luke Quinn <lquinn@vt.edu>
2024-01-10 09:28:35 -0500

    atags are passed if you run make qemu and attach the sd card file drives

--- kern/src/allocator/util.rs
+++ kern/src/allocator/util.rs
@@ -6,7 +6,7 @@
 ///
 /// Panics if `align` is not a power of 2.
 pub fn align_down(addr: usize, align: usize) -> usize {
-    assert!(!is_power_of_two(align));
+    assert!(is_power_of_two(align));
     addr & !(addr - 1)
 }
 
@@ -19,9 +19,11 @@ pub fn align_down(addr: usize, align: usize) -> usize {
 /// Panics if `align` is not a power of 2
 /// or aligning up overflows the address.
 pub fn align_up(addr: usize, align: usize) -> usize {
-    assert!(!is_power_of_two(align));
+    assert!(is_power_of_two(align));
     addr.checked_add(align - 1).unwrap() & !(align - 1)
 }
+
+use crate::kprintln;
 
 fn is_power_of_two(val: usize) -> bool {
     (val & (val - 1)) == 0
